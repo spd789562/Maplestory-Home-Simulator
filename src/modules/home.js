3133346413ec10b5a51d2d17d5468b0de6af7bb7
spd789562 <leo.yicun.lin@gmail.com>
2021-01-28 15:55:58 +0800

    [edit] default theme function not work

--- src/modules/home.js
+++ src/modules/home.js
@@ -4,7 +4,7 @@ import { getTheme } from '@mapping/map-theme/name'
 
 /* utils */
 import { entries } from '@utils/ramda'
-import { fromPairs, map, propEq } from 'ramda'
+import { fromPairs, map, propEq, unnest, values } from 'ramda'
 
 class Home {
   constructor(selectId) {
@@ -13,10 +13,12 @@ class Home {
     this.selectId = selectId
     this.mapId = currentMapData.templateMapID
     this.theme = fromPairs(
-      map(
-        ({ 0: { info, itemID, ...themeObject } }) =>
-          map(({ name, state }) => [name, state], themeObject),
-        currentMapData.themeable
+      unnest(
+        map(
+          ({ 0: { info, itemID, ...themeObject } }) =>
+            map(({ name, state }) => [name, state], values(themeObject)),
+          values(currentMapData.themeable)
+        )
       )
     )
     this.furnitures = []

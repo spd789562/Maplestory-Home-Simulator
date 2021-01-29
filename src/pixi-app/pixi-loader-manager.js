b275d97dc10aa495241a864c929203d30a8c2f80
spd789562 <leo.yicun.lin@gmail.com>
2021-01-29 10:23:47 +0800

    [edit] use new loader instead of app loader

--- src/pixi-app/pixi-loader-manager.js
+++ src/pixi-app/pixi-loader-manager.js
@@ -1,5 +1,8 @@
+import { Loader } from 'pixi.js-legacy'
+
 /* utils */
-import { pipe, filter, uniq } from 'ramda'
+import { pipe, filter, uniq, keys } from 'ramda'
+import { entries } from '@utils/ramda'
 import getConfig from 'next/config'
 
 const { IMAGE_CDN } = getConfig().publicRuntimeConfig
@@ -7,56 +10,59 @@ const { IMAGE_CDN } = getConfig().publicRuntimeConfig
 class PixiLoaderManager {
   constructor(app) {
     this.app = app
-    this.task = []
-    this.isLoaderRunning = false
+    this.task = {}
   }
   load(src, callback) {
-    const taskId = Symbol('loader-task-id')
-    this.task.push({
-      id: taskId,
-      src,
-      callback,
-    })
-    this.checkTask()
-  }
-  reset() {
-    this.isLoaderRunning = false
-    this.app.loader.reset()
-    this.task = []
-  }
-  checkTask() {
-    !this.isLoaderRunning && this.runTask()
-  }
-  runTask() {
-    if (!this.task.length) {
-      this.isLoaderRunning = false
+    const needLoadSrc = this.getNeedLoadSrc(src)
+    if (!needLoadSrc.length) {
+      callback()
       return
     }
-    this.isLoaderRunning = true
-    const currentTask = this.task.pop()
-    const needLoadSrc = pipe(
+    const taskId = Symbol('loader-task-id')
+    this.task[taskId] = {
+      src: needLoadSrc,
+      callback,
+    }
+    this.runTask(taskId)
+  }
+  reset() {
+    keys(this.task).forEach((id) => {
+      this.task[id].cancel = true
+    })
+  }
+  getNeedLoadSrc(srcs) {
+    return pipe(
       filter((src) => !this.app.loader.resources[src]),
       uniq
-    )(currentTask.src)
-    if (needLoadSrc.length) {
-      this.app.loader
-        .add(
-          needLoadSrc.map((url) => ({
-            name: url,
-            url:
-              !url.startsWith('/') || process.env.NODE_ENV === 'development'
-                ? url
-                : `${IMAGE_CDN}${location.host}${url}`,
-          }))
-        )
-        .load(() => {
+    )(srcs)
+  }
+  addResource(resources) {
+    entries(([key, res]) => {
+      if (!this.app.loader.resources[key]) {
+        this.app.loader.resources[key] = res
+      }
+    }, resources)
+  }
+  runTask(taskId) {
+    const currentTask = this.task[taskId]
+    this.task[taskId].loader = new Loader()
+    this.task[taskId].loader
+      .add(
+        currentTask.src.map((url) => ({
+          name: url,
+          url:
+            !url.startsWith('/') || process.env.NODE_ENV === 'development'
+              ? url
+              : `${IMAGE_CDN}${location.host}${url}`,
+        }))
+      )
+      .load((ld, resources) => {
+        this.addResource(resources)
+        if (!this.task[taskId].loader.cancel) {
           currentTask.callback()
-          this.runTask()
-        })
-    } else {
-      currentTask.callback()
-      this.runTask()
-    }
+        }
+        delete this.task[taskId]
+      })
   }
 }
 

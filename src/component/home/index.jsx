4eb77fa7ff82af5a5a576ba1d3e90fb12d9d7d60
spd789562 <leo.yicun.lin@gmail.com>
2021-02-22 15:39:59 +0800

    [add] connect store and adapt furniture place function

--- src/component/home/index.jsx
+++ src/component/home/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, createRef, useRef } from 'react'
 
 /* store */
 import { useStore } from '@store'
+import { CLEAR_ACTIVE_FURNITURE } from '@store/active-furniture'
 
 /* components */
 import PixiAPP from '../../pixi-app'
@@ -12,9 +13,10 @@ const appRef = createRef()
 const ESC_KEY_CODE = 27
 
 const Home = () => {
-  const [currentIndex] = useStore('house.current')
+  const [currentIndex, dispatch] = useStore('house.current')
   const [edit] = useStore('meta.edit')
   const [currentHomeData] = useStore(`house.houses.${currentIndex}`)
+  const [activeFurnitureID] = useStore('active-furniture')
   const onEsc = ({ keyCode }) => {
     if (keyCode === ESC_KEY_CODE) {
       appRef.current &&
@@ -22,9 +24,13 @@ const Home = () => {
         appRef.current.activeFurniture.cancelDrag()
     }
   }
+  const onCancelFurniture = () => {
+    dispatch({ type: CLEAR_ACTIVE_FURNITURE })
+  }
   useEffect(() => {
     if (canvasRef.current) {
       appRef.current = new PixiAPP(canvasRef.current)
+      appRef.current.cancelPlaceFurniture = onCancelFurniture
       window.addEventListener('keydown', onEsc)
     }
     return () => {
@@ -46,6 +52,13 @@ const Home = () => {
       app.isEdit = edit
     }
   }, [appRef.current, edit])
+  useEffect(() => {
+    const app = appRef.current
+    if (app && activeFurnitureID) {
+      app.isEdit = true
+      app.placeNewFurniture(activeFurnitureID)
+    }
+  }, [appRef.current, activeFurnitureID])
 
   return <canvas ref={canvasRef} style={{ userSelect: 'none' }} />
 }

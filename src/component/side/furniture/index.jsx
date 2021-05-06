290f3c97e5d516b8765de140cb6eb8f402966374
spd789562 <leo.yicun.lin@gmail.com>
2021-05-06 18:14:03 +0800

    [add] furniture tab

--- src/component/side/furniture/index.jsx
+++ src/component/side/furniture/index.jsx
@@ -5,7 +5,7 @@ import { useStore } from '@store'
 import { withTranslation } from '@i18n'
 
 /* components */
-import { Row, Col } from 'antd'
+import { Row, Col, Tabs } from 'antd'
 import Item from './item'
 
 /* utils */
@@ -15,13 +15,32 @@ import { flatten, keys, map, path, pipe, tap, uniq, values } from 'ramda'
 import FurnitureMapping from '@mapping/furniture'
 
 const Furniture = ({ t }) => {
+  const [favoritesFurniture] = useStore('favorite-furniture')
   return (
-    <Row gutter={[8, 8]}>
-      {keys(FurnitureMapping).map((id) => (
-        <Col span={6} key={`furniture-select-${id}`}>
-          <Item id={id} />
-        </Col>
-      ))}
+    <Row style={{ marginTop: -20 }}>
+      <Col flex="1">
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab={t('tab_all')} key="1">
+            {/* add padding prevent TabPane overflow hidden overlay favorite star */}
+            <Row gutter={[8, 8]} style={{ paddingRight: 6 }}>
+              {keys(FurnitureMapping).map((id) => (
+                <Col span={6} key={`furniture-select-${id}`}>
+                  <Item id={id} />
+                </Col>
+              ))}
+            </Row>
+          </Tabs.TabPane>
+          <Tabs.TabPane tab={t('tab_favorite')} key="2">
+            <Row gutter={[8, 8]} style={{ paddingRight: 6 }}>
+              {favoritesFurniture.map((id) => (
+                <Col span={6} key={`furniture-select-${id}`}>
+                  <Item id={id} />
+                </Col>
+              ))}
+            </Row>
+          </Tabs.TabPane>
+        </Tabs>
+      </Col>
     </Row>
   )
 }

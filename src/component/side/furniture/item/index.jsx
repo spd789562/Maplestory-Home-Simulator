678fb8784b167ebacb825ad137eed72611952af2
spd789562 <leo.yicun.lin@gmail.com>
2021-05-11 16:03:42 +0800

    [edit] all poster use real image not icon

--- src/component/side/furniture/item/index.jsx
+++ src/component/side/furniture/item/index.jsx
@@ -9,13 +9,30 @@ import { CHANGE_ACTIVE_FURNITURE } from '@store/active-furniture'
 import DetailPopover from './detail-popover'
 import Favorite from './favorite'
 
+/* utils */
+import { defaultTo, equals, findIndex, path, pipe, values } from 'ramda'
+
+/* mapping */
+import { filteredFurniture as FurnitureMapping } from '@mapping/furniture'
+
 import styles from './item.scss'
+
+const isPoster = (furniture) =>
+  pipe(
+    path(['info', 'tag']),
+    defaultTo({}),
+    values,
+    findIndex(equals('poster')),
+    (index) => index !== -1
+  )(furniture)
 
 const Item = ({ t, id }) => {
   const dispatch = useDispatch()
   const handleClick = () => {
     dispatch({ type: CHANGE_ACTIVE_FURNITURE, payload: id })
   }
+  const furniture = FurnitureMapping[id] || {}
+  const _isPoster = isPoster(furniture)
   return (
     <div className={styles['item']}>
       <Favorite
@@ -32,7 +49,9 @@ const Item = ({ t, id }) => {
         <div
           className={styles['item-image']}
           style={{
-            backgroundImage: `url(/furniture/Item-Consume-0267.img-${id}-info-icon.png)`,
+            backgroundImage: `url(/furniture/Item-Consume-0267.img-${id}${
+              _isPoster ? '-layer1-0' : '-info-icon'
+            }.png)`,
           }}
           onClick={handleClick}
         />

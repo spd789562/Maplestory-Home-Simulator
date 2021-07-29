7c4a0408bba30381fbe7d2ac0aa680a2e2a67742
spd789562 <yicun19990403@gmail.com>
2021-07-29 19:39:40 +0800

    [add] new template and image

--- src/mapping/map-theme/name.js
+++ src/mapping/map-theme/name.js
@@ -41,6 +41,27 @@ export const getTheme = (mapId) => {
     case '871100015':
     case '871100016':
       return 'christmax'
+    case '018':
+    case '019':
+    case '020':
+    case '871100017':
+    case '871100018':
+    case '871100019':
+      return 'henesysExpand'
+    case '021':
+    case '022':
+    case '023':
+    case '871100020':
+    case '871100021':
+    case '871100022':
+      return 'commerzExpand'
+    case '024':
+    case '025':
+    case '026':
+    case '871100023':
+    case '871100024':
+    case '871100025':
+      return 'lithharborExpand'
     default:
       return ''
   }
@@ -55,18 +76,26 @@ export const getType = (mapId) => {
     case '871100004':
     case '871100008':
     case '871100012':
+    case '871100017':
+    case '871100020':
+    case '871100024':
       return 'right_curve_stair'
     case '871100013':
     case '871100014':
       return 'right_stair'
     case '871100002':
     case '871100005':
+    case '871100018':
+    case '871100021':
       return 'left_stair'
     case '871100003':
     case '871100006':
     case '871100009':
     case '871100013':
     case '871100015':
+    case '871100022':
+    case '871100019':
+    case '871100025':
       return 'ladder'
     case '871100007':
       return 'center_stair'
